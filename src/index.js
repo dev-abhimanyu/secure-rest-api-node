b1d462ef2e7f5f92f4438756814b0ec4bc3b4c51
dev-abhimanyu <abhimanyusharma98@gmail.com>
2023-01-13 00:53:27 -0500

    added mongo

--- src/index.js
+++ src/index.js
@@ -3,32 +3,37 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helment = require('helmet');
 const morgan = require('morgan');
+const {startDatabase} = require('./database/mongo');
+const {insertAd, getAds} = require('./database/ads');
 
 const PORT = 3001;
 
 // defining the Express app
 const app = express();
 
-// defning an array to work as the database
-const ads = [
-    {title : 'hello, world (again)!'}
-]
-
 // adding helmet to enhance REST API's security
 app.use(helment());
 // using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser());
+app.use(bodyParser.json());
 // enabling CORS for all requests
 app.use(cors());
 // adding morgan to log HTTP requests
-app.use(morgan());
+app.use(morgan('combined'));
 
 
 // defining endpoint to return data
-app.get('/', (req,res) => {
-    res.send(ads);
+app.get('/', async(req,res) => {
+    res.send(await getAds);
 })
 
-app.listen(PORT, () => {
-    console.log('Listening on port ', PORT);
-})
+// start the in-memory MongoDB instance
+startDatabase().then(async() =>{
+    
+    await insertAd({title: 'Hello, now from the in-memory database!'});
+
+    // start the server
+    app.listen(PORT, async() => {
+        console.log('Listening on port ', PORT);
+    });
+});
+
